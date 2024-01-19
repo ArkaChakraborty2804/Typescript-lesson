52676b9fd656110514caaba744457340812e62b1
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-19 23:42:19 +0530

    second commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+let myName = 'Arka';
+let sport;
+sport = 'Cricket';
+sport = 'Football';
+let meaningOfLife;
+let isLoading;
+meaningOfLife = 42;
+isLoading = false;
+let album;
+album = 'true';
+album = 3;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+let myName: string = 'Arka'
+
+let sport: string
+
+sport = 'Cricket'
+sport = 'Football'
+
+let meaningOfLife: number
+let isLoading: boolean
+
+meaningOfLife = 42
+isLoading = false
+
+let album: string | number
+album = 'true'
+album = 3
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean | string
+
+let re: RegExp = /\w+/g
